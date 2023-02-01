2709b502dde82894d3045918ca71d5836ed26eae
Peter Umpleby <Peter.Umpleby@tracerco.com>
2023-02-01 19:51:35 +0000

    added http trigger

---
+++ TomLonghurst.PullRequestScanner.FunctionApp/HttpTrigger.cs
@@ -0,0 +1,33 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using TomLonghurst.PullRequestScanner.Services;
+
+namespace TomLonghurst.PullRequestScanner.FunctionApp
+{
+    public class HttpTrigger
+    {
+        private readonly IPullRequestScanner _pullRequestScanner;
+
+        public HttpTrigger(IPullRequestScanner pullRequestScanner)
+        {
+            _pullRequestScanner = pullRequestScanner;
+        }
+
+        [FunctionName("HttpTrigger")]
+        public async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            await _pullRequestScanner.ExecutePluginsAsync();
+
+            return new OkResult();
+        }
+    }
+}
